b6c7ec1a2e97839f6eb1cb3cddaa7114389aab68
Julie Nisbet <jmnisbet@gmail.com>
2023-01-17 11:18:42 -0800

    first useEffect hook

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
-import { animals } from '../../data';
+// import { animals } from '../../data';
 import Animal from '../Animal/Animal';
 import background from '../../background.png';
+import { fetchAnimals } from '../../services/animals';
 
 export default function Main() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchAnimals();
+      setAnimals(resp);
+    };
+    fetchData();
+  }, []);
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
