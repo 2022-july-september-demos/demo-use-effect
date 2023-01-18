939dfc5db620c81ed5d7aaf6bdf985d601006e3b
Julie Nisbet <jmnisbet@gmail.com>
2023-01-18 14:02:41 -0800

    add try/catch and filter demo

--- src/hooks/useAnimals.js
+++ src/hooks/useAnimals.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { fetchAnimals } from '../services/animals';
 export function useAnimals() {
   const [animals, setAnimals] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const resp = await fetchAnimals();
-      setAnimals(resp);
+      try {
+        const resp = await fetchAnimals();
+        setAnimals(resp);
+      } catch (e) {
+        setError('Uh oh :( Something went wrong!');
+      }
     };
     fetchData();
   }, []);
-  return animals;
+  return { animals, error };
 }
